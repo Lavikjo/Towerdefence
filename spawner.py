665e6ab41fbaf21625599ea957eabd6238e32e53
Joel Lavikainen <joel.lavikainen@aalto.fi>
2017-03-21 14:19:54 +0200

    Add spawner class

---
+++ spawner.py
@@ -0,0 +1,35 @@
+from threading import Timer
+from random import Random, choice
+
+class Spawner():
+
+
+	def __init__(self, wave, spawn_delay, seed):
+		self.wave = wave
+		self.complete = False
+		self.random random.Random(seed)
+		self.spawntimer = Timer(spawn_delay, self.spawn_enemy())
+		self.spawntimer.start()
+
+	def next_enemy(self):
+		'''
+		Returns next random enemy from wave
+		'''
+		# generate wave dictionary with only positive amounts
+		wave = {key: value for (key, value) in self.wave if value}
+
+		if wave:
+			enemy_type, amount = self.random.choice(list(self.wave.keys()))
+		else:
+			# no more enemies left so wave is complete
+			self.complete = True
+
+	def spawn_enemy(self):
+		'''
+		Spawns next random enemy to field and starts spawntimer until wave is complete
+		'''
+		enemy_type = self.next_enemy()
+
+		if not self.complete:
+			self.wave[enemy_type] -= 1
+			self.spawntimer.start()
